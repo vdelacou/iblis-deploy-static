39fccb43c8dbb9bcb8db1bb6e609ba06b0addd6a
Vincent Delacourt <vincent.delacourt@adama-development.com>
2019-03-21 20:08:55 +0800

    typo in serverless command

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 npm install
 npm install -g serverless
 serverless deploy -v
-export S3_BUCKET=$(severless info -v | grep BucketSiteName | sed s/BucketSiteName\:\ //g | awk '{print $1}')
-export CLOUDFRONT_DISTRIBUTION_ID=$(severless info -v | grep CloudFrontDistributionId | sed s/CloudFrontDistributionId\:\ //g | awk '{print $1}')
+export S3_BUCKET=$(serverless info -v | grep BucketSiteName | sed s/BucketSiteName\:\ //g | awk '{print $1}')
+export CLOUDFRONT_DISTRIBUTION_ID=$(serverless info -v | grep CloudFrontDistributionId | sed s/CloudFrontDistributionId\:\ //g | awk '{print $1}')
 pip install awscli --upgrade --ignore-installed six
 aws s3 sync build/ s3://$S3_BUCKET --delete --cache-control max-age=31536000,public
 aws s3 cp s3://$S3_BUCKET/index.html s3://$S3_BUCKET/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html
